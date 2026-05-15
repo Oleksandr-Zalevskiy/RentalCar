2f10c9b7ed60f3effe69ebaead26f3e807784919
zalevskiy <zaleva100@gmail.com>
2026-05-15 21:51:24 +0300

    add axios api layer

---
+++ my-app/lib/api/cars.ts
@@ -0,0 +1,37 @@
+import { api } from './axios';
+import { CarsResponse } from '@/types/car';
+
+interface FetchCarsParams {
+  pageParam?: number;
+  brand?: string;
+  rentalPrice?: string;
+  minMileage?: string;
+  maxMileage?: string;
+}
+
+export const fetchCars = async ({
+  pageParam = 1,
+  brand,
+  rentalPrice,
+  minMileage,
+  maxMileage,
+}: FetchCarsParams): Promise<CarsResponse> => {
+  const { data } = await api.get('/cars', {
+    params: {
+      page: pageParam,
+      limit: 12,
+      brand,
+      rentalPrice,
+      minMileage,
+      maxMileage,
+    },
+  });
+
+  return data;
+};
+
+export const fetchCarById = async (id: string) => {
+  const { data } = await api.get(`/cars/${id}`);
+
+  return data;
+};
